dec99d1dadaddd252028012154b1083188030010
Дмитрий Ценеков <49730476+DimaThenekov@users.noreply.github.com>
2024-11-01 16:10:34 +0300

    Update and rename HelloWorldBench.c to TestLoader.c

--- benchmark/HelloWorldBench.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <windows.h>
-
-int main() {
-    printf("Sleep 5.5s...\n");
-    Sleep(5500);
-    printf("Done\n");
-    return 0;
-}
---
+++ benchmark/TestLoader.c
@@ -0,0 +1,11 @@
+// Copyright 2024 Loader
+
+#include <cstdio>
+#include <windows.h>
+
+int main() {
+    printf("Sleep 5.5s...n");
+    Sleep(5500);
+    printf("Donen");
+    return 0;
+}
